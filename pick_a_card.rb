0ef2862672ea774050245095b92bed3d81f9da0c
zubikarai <7schets7@gmail.com>
2022-04-13 14:26:40 +0500

    Alternative output

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,11 +1,11 @@
 puts "Hello"
 values = %w[2 3 4 5 6 7 8 9 10 J Q K A]
-suits = %w[♦ ♥ ♣ ♠]
+suits = %w[Diamonds Hearts Clubs Spades]
 
 cards = []
 values.each do |value|
   suits.each do |suit|
-    cards << "#{value} of #{suit}"
+    cards << "#{value}#{suit[0].downcase}"
   end
 end
 
